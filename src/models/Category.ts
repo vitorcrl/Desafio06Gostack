66c42f65929f9d3acf16bdab5c84f48c2ab1a3cb
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-12-31 03:36:37 -0300

    Commited:modified migrations and model rf and db

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,26 @@
+import { Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn }
+  from 'typeorm';
+
+
+@Entity('category')
 class Category {
+
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,39 @@
+import { Entity,
+         Column,
+        PrimaryGeneratedColumn,
+        CreateDateColumn,
+        UpdateDateColumn,
+        ManyToOne,
+      } from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transaction')
 class Transaction {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
+  @ManyToOne(()=> Category)
+  @JoinColumn({name:'category_id'})
+  category_id: Category;
 
+
+  @CreateDateColumn()
   created_at: Date;
 
+
+  @UpdateDateColumn()
   updated_at: Date;
-}
+};
 
 export default Transaction;
